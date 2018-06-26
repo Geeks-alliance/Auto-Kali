8e9b1bce32e63e11b609a33f4f1526991eaa086a
{“text”：“json”} <36883093+nios34@users.noreply.github.com>
2018-06-26 13:15:19 +0800

    Update master.py

--- master.py
+++ master.py
@@ -20,7 +20,7 @@ print (colored('''   mm            m                  #             ""#      "
   #  #  #   #    #    #" "#         # m"   "   #    #      #   
   #mm#  #   #    #    #   #   """   #"#    m"""#    #      #   
  #    # "mm"#    "mm  "#m#"         #  "m  "mm"#    "mm  mm#mm ''',"green"))
-print (colored('\n##################################################\n本作品遵循GNU协议条款\n制作者：极客之眼团队_{"text":"json"}\n极客之眼团队群号：659155551\n本工具只适用与kali linux中\n如有错误请联系QQ：1945649519\n可以输入help查看帮助\ngithub仓库:https://github.com/nios34/kali-Tools\n###########最后祝您使用愉快#######################',"green"))
+print (colored('\n##################################################\n本作品遵循GNU协议条款\n制作者：极客之眼团队_{"text":"json"}\n极客之眼团队群号：659155551\n本工具只适用与kali linux中\n如有错误请联系QQ：1945649519\n可以输入help查看帮助\ngithub仓库:https://github.com/nios34/Auto-Kali\n###########最后祝您使用愉快#######################',"green"))
 XX= input(colored('请写出您要执行的命令:',"yellow"))
 if XX == "help":
     print ("          install 安装程序")
