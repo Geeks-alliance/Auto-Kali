bf31ce28e0a54adfa2ad90c62f7cee66b1d8058f
{"text":"json"} <gongzhile2905@gmail.com>
2018-06-28 13:48:17 +0800

    fixu

--- scrips/install.sh
+++ scrips/install.sh
@@ -6,10 +6,10 @@ if [ "`id -u`" != 0 ]; then
 fi
 
 if (whiptail --title "AUTO-KALI STSRT"  --yesno '   mm            m                  #             ""#      "   \n   ##   m   m  mm#mm   mmm          #   m   mmm     #    mmm   \n  #  #  #   #    #    #" "#         # m"   "   #    #      #   \n  #mm#  #   #    #    #   #   """   #"#    m"""#    #      #   \n #    # "mm"#    "mm  "#m#"         #  "m  "mm"#    "mm  mm#mm\n\n                       是否安装Auto-kali?' 15 68) then
-    exec apt-get install git python3 python3-termcolor -y
-    exec git clone https://github.com/nios34/Auto-kali /usr/share/auto-kali
-    exec ln -s /usr/share/auto-kali/START /bin/auto-kali
-    exec chmod 777 /bin/auto-kali
+    apt-get install git python3 python3-termcolor -y
+    git clone https://github.com/nios34/Auto-kali /usr/share/auto-kali
+    ln -s /usr/share/auto-kali/START /bin/auto-kali
+    chmod 777 /bin/auto-kali
     whiptail --title "Install finish" --msgbox "安装完成!    启动指令:auto-kali" 7 60
     exit
 else
