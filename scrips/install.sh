7ec6ccd2b56d328ca43cb1ad668871f3713075a3
{“text”：“json”} <36883093+nios34@users.noreply.github.com>
2018-07-26 19:18:58 +0800

    Update install.sh

--- scrips/install.sh
+++ scrips/install.sh
@@ -7,6 +7,7 @@ fi
 
 if (whiptail --title "AUTO-KALI STSRT"  --yesno '   mm            m                  #             ""#      "   \n   ##   m   m  mm#mm   mmm          #   m   mmm     #    mmm   \n  #  #  #   #    #    #" "#         # m"   "   #    #      #   \n  #mm#  #   #    #    #   #   """   #"#    m"""#    #      #   \n #    # "mm"#    "mm  "#m#"         #  "m  "mm"#    "mm  mm#mm\n\n                       是否安装Auto-kali?' 15 68) then
     apt install wget git python3 python3-termcolor -y
+    echo "python3 /usr/share/auto-kali/master.py" > /usr/local/Auto-kali-path.conf
     git clone https://github.com/nios34/Auto-kali /usr/share/auto-kali
     wget -P /bin https://raw.githubusercontent.com/nios34/Auto-Kali/master/scrips/auto-kali && chmod 777 /bin/auto-kali && whiptail --title "Install finish" --msgbox "安装完成!    启动指令:auto-kali" 7 60
 else
