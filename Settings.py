a0f60806f141ad44f91cd9fd6b84b53038701227
{"text":"json"} <gongzhile2905@gmail.com>
2018-08-06 21:10:41 +0800

    Auto update

--- Settings.py
+++ Settings.py
@@ -14,7 +14,11 @@ def main():
 """))
     XX = input(colored("选项:","yellow"))
     if XX == "Update":
-        os.system("cd /usr/share/auto-kali && git pull")
+        f = open("/usr/share/auto-kali/Auto-kali-path.conf")
+        p = f.read()
+        p = str(p)
+        p = p.strip("python3 ")
+        os.system("cd %s && git pull" % p)
         print (colored("操作完成，3秒后重启脚本","blue"))
         time.sleep(3)
         os.system("auto-kali")
