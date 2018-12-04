441876fcdd7c8684dae94c0bd3e49cc5df7d20e9
{“text”：“json”} <36883093+nios34@users.noreply.github.com>
2018-12-04 12:36:50 +0800

    Update install.sh

--- scrips/install.sh
+++ scrips/install.sh
@@ -7,10 +7,10 @@ fi
 
 if (whiptail --title "AUTO-KALI STSRT"  --yesno '   mm            m                  #             ""#      "   \n   ##   m   m  mm#mm   mmm          #   m   mmm     #    mmm   \n  #  #  #   #    #    #" "#         # m"   "   #    #      #   \n  #mm#  #   #    #    #   #   """   #"#    m"""#    #      #   \n #    # "mm"#    "mm  "#m#"         #  "m  "mm"#    "mm  mm#mm\n\n                       是否安装Auto-kali?' 15 68) then
     apt install wget git python3 python3-termcolor -y
-    git clone https://github.com/auto-kali/Auto-kali /usr/share/auto-kali
+    git clone https://github.com/Geeks-alliance/Auto-kali /usr/share/auto-kali
     echo "/usr/share/auto-kali" > /usr/share/auto-kali/Auto-kali-path.conf
     echo "import termcolor" > /usr/share/auto-kali/test.py
-    wget -P /bin https://raw.githubusercontent.com/auto-kali/Auto-Kali/master/scrips/auto-kali && chmod 777 /bin/auto-kali && whiptail --title "Install finish" --msgbox "安装完成!    启动指令:auto-kali" 7 60
+    wget -P /bin https://raw.githubusercontent.com/Geeks-alliance/Auto-Kali/master/scrips/auto-kali && chmod 777 /bin/auto-kali && whiptail --title "Install finish" --msgbox "安装完成!    启动指令:auto-kali" 7 60
 else
     exit
 fi
