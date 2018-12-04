7fea7fb44f14a88f061d83816bb911ffe70f184b
{“text”：“json”} <36883093+nios34@users.noreply.github.com>
2018-12-04 12:45:15 +0800

    Update master.py

--- master.py
+++ master.py
@@ -68,5 +68,5 @@ if __name__ == '__main__':
   #  #  #   #    #    #" "#         # m"   "   #    #      #   
   #mm#  #   #    #    #   #   """   #"#    m"""#    #      #   
  #    # "mm"#    "mm  "#m#"         #  "m  "mm"#    "mm  mm#mm ''',"green"))
-    print (colored('\n##################################################\n本作品遵循GNU协议条款\n制作者：极客之眼团队_{"text":"json"}_无忧Parker\n极客之眼团队群号：659155551\n本工具只适用与kali linux中\n如有错误请联系QQ：1945649519\n可以输入help查看帮助\ngithub仓库:https://github.com/Geeks-alliance/Auto-Kali\n###########最后祝您使用愉快#######################',"green"))
+    print (colored('\n##################################################\n本作品遵循GNU协议条款\n制作者：{"text":"json"}_无忧Parker\n极客之眼团队群号：659155551\n本工具只适用与kali linux中\n如有错误请联系QQ：1945649519\n可以输入help查看帮助\ngithub仓库:https://github.com/Geeks-alliance/Auto-Kali\n###########最后祝您使用愉快#######################',"green"))
     main()
